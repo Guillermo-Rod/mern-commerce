6fa5dbac43bf9124377e804b9b5a05964b290fff
Guillermo Rodriguez <guillermo.rodriguez.cv@gmail.com>
2024-06-17 04:54:39 -0600

    chore: add loginController

---
+++ backend/src/app/controllers/auth/loginController.mjs
@@ -0,0 +1,24 @@
+import { createUser } from "../../services/userService.mjs";
+import ResponseService from "../../utils/ResponseService.mjs";
+import mapValidationErrors from "../../utils/mapValidationErrors.mjs";
+
+export const signup = async (req, res) => {
+    const response = new ResponseService(res);
+    
+    try {
+        const user = await createUser(req.body)
+        response.created('User', {user: user})
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            response.badRequest('Validation error', {
+                exception_name: error.name,
+                error: error.message, 
+                errors: mapValidationErrors(error.errors),
+            });
+        }else if (error.name === 'ModelAlreadyExists') {
+            response.conflict(error.message, {exception_name: error.name});
+        }else {
+            response.internalServerError(error.message);
+        }
+    }
+}
