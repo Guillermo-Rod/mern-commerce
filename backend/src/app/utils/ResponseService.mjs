895d969d4576b8afd421d5e5db60694c99451199
Guillermo Rodriguez <guillermo.rodriguez.cv@gmail.com>
2024-06-19 13:27:49 -0600

    chore: add static codes to ResponseService

--- backend/src/app/utils/ResponseService.mjs
+++ backend/src/app/utils/ResponseService.mjs
@@ -1,5 +1,16 @@
 class ResponseService {
-    
+    static OK_CODE = 200;
+    static RESOURCE_CREATED_CODE = 201;
+    static NO_CONTENT_CODE = 204;
+    static NOT_MODIFIED_CODE = 304;
+    static BAD_REQUEST_CODE = 400;
+    static UNAUTHORIZED_CODE = 401;
+    static FORBIDDEN_CODE = 403;
+    static NOT_FOUND_CODE = 404;
+    static CONFLICT_CODE = 409;
+    static INTERNAL_ERROR_CODE = 500;
+    static SERVICE_UNAVAILABLE_CODE = 503;
+
     constructor(response) {
         this.response = response;
     }
@@ -12,14 +23,14 @@ class ResponseService {
     // 200 OK
     // Action was successfully executed
     ok (message = 'OK', data) {
-        const statusCode = 200;
+        const statusCode = ResponseService.OK_CODE;
         this.response.status(statusCode).json(this.addToJson(data, {message, statusCode}));
     }
 
     // 201 Resource Created
     // Action was successfully executed and resource was created
     created(model, data) {
-        const statusCode = 201;
+        const statusCode = ResponseService.RESOURCE_CREATED_CODE;
         const message = `${model} registered successfully`;
         this.response.status(statusCode).json(this.addToJson(data, {message, statusCode}));
     }
@@ -27,19 +38,19 @@ class ResponseService {
     // 204 No Content
     // Action was successfully executed but not exists content to return
     noContent() {
-        this.response.status(204).send({});
+        this.response.status(ResponseService.NO_CONTENT_CODE).send({});
     }
 
     // 304 Not Modified
     // The info has not been changed since the latest request
     notModified() {
-        this.response.status(304).send({}); 
+        this.response.status(ResponseService.NOT_MODIFIED_CODE).send({}); 
     }
 
     // 400 Bad Request
     // The request is not valid or is malformed
     badRequest(reason, data) {
-        const statusCode = 400;
+        const statusCode = ResponseService.BAD_REQUEST_CODE;
         const message = `Bad Request : ${reason}`;
         this.response.status(statusCode).json(this.addToJson(data, {message, statusCode}));
     }
@@ -47,7 +58,7 @@ class ResponseService {
     // 401 Unauthorized
     // User not authenticated
     unauthorized(data) {
-        const statusCode = 401;
+        const statusCode = ResponseService.UNAUTHORIZED_CODE;
         const message = 'Authentication is required';
         this.response.status(statusCode).json(this.addToJson(data, {message, statusCode}));
     }
@@ -55,7 +66,7 @@ class ResponseService {
     // 403 Forbidden
     // User authenticated but without permissions
     forbidden(data) {
-        const statusCode = 403;
+        const statusCode = ResponseService.FORBIDDEN_CODE;
         const message = 'User authenticated but without permissions';
         this.response.status(statusCode).json(this.addToJson(data, {message, statusCode}));
     }
@@ -63,7 +74,7 @@ class ResponseService {
     // 404 Not Found
     // The requested resource not found
     notFound(resource, data) {
-        const statusCode = 404;
+        const statusCode = ResponseService.NOT_FOUND_CODE;
         const message = `Not Found : ${resource}`;
         this.response.status(statusCode).json(this.addToJson(data, {message, statusCode}));
     }
@@ -71,7 +82,7 @@ class ResponseService {
     // 409 Conflict
     // The request was not completed because there are an conflict with his current state 
     conflict(reason, data) {
-        const statusCode = 409;
+        const statusCode = ResponseService.CONFLICT_CODE;
         const message = `Conflict : ${reason}`;
         this.response.status(statusCode).json(this.addToJson(data, {message, statusCode}));
     }
@@ -79,7 +90,7 @@ class ResponseService {
     // 500 Server error
     // Code error, server error
     internalServerError(reason, data) {
-        const statusCode = 500;
+        const statusCode = ResponseService.INTERNAL_ERROR_CODE;
         const message = `Server error : ${reason}`;
         this.response.status(statusCode).json(this.addToJson(data, {message, statusCode}));
     }
@@ -87,7 +98,7 @@ class ResponseService {
     // 503 Service Unavailable
     // The server is not available to handle the request
     serviceUnavailable(reason, data) {
-        const statusCode = 503;
+        const statusCode = ResponseService.SERVICE_UNAVAILABLE_CODE;
         const message = `Service Unavailable : ${reason}`;
         this.response.status(statusCode).json(this.addToJson(data, {message, statusCode}));
     }
