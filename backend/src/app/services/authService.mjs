73450ecf62d3a92962c24bd050142bf605898c2d
Guillermo Rodriguez <guillermo.rodriguez.cv@gmail.com>
2024-06-19 14:09:04 -0600

    chore: add login method in authService

--- backend/src/app/services/authService.mjs
+++ backend/src/app/services/authService.mjs
@@ -1,5 +1,11 @@
+import 'dotenv/config';
+import AuthValidationError from "../exceptions/AuthValidationError.mjs";
 import ModelAlreadyExists from "../exceptions/ModelAlreadyExistsException.mjs";
+import ModelNotFoundError from "../exceptions/ModelNotFoundError.mjs";
 import User from "../models/User.mjs";
+import isEmail from 'validator/lib/isEmail';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 /**
  * Store User in Database
@@ -20,4 +26,23 @@ export async function signupUser ({name, last_name, phone, email, password}) {
     await user.save();
 
     return user;
+}
+
+export async function login (email, password) {
+    
+    if (! email || ! password) throw AuthValidationError.missingRequiredParameters('email', 'password');
+    if (! isEmail(email)) throw AuthValidationError.invalidParameterFormat('email', email); 
+    
+    const user = await User.findOne({ email });
+
+    if (! user) throw new ModelNotFoundError('User');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (! isMatch) throw AuthValidationError.wrongPassword();
+
+    // Make Auth Token
+    const token = jwt.sign({ id: user._id, email: user.email }, process.env.APP_KEY, {expiresIn: '1h'});
+
+    return token;
 }
