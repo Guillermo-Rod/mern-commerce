63bc7240126a457c113c2f03cb03c24c3dcba068
Guillermo Rodriguez <guillermo.rodriguez.cv@gmail.com>
2024-06-21 17:12:25 -0600

    chore: add /user-info endpoint

--- backend/src/routes/web.mjs
+++ backend/src/routes/web.mjs
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { signup, login, refreshAuthToken } from '../app/controllers/auth/loginController.mjs';
-import User from '../app/models/User.mjs';
+import { show as showUser } from '../app/controllers/userController.mjs';
 
 const router = new Router();
 
 router.post('/login', login);
 router.post('/signup', signup);
-router.post('/refresh-token', refreshAuthToken)
+router.post('/refresh-token', refreshAuthToken);
+router.get('/user-info', showUser);
 
 export default router;
