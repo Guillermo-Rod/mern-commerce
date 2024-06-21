d529ee4c1e2fa496cd5acfb54aa55e636086aadc
Guillermo Rodriguez <guillermo.rodriguez.cv@gmail.com>
2024-06-21 17:09:35 -0600

    chore: add getUser method to userService

--- backend/src/app/services/userService.mjs
+++ backend/src/app/services/userService.mjs
@@ -1,4 +1,6 @@
-import ModelAlreadyExists from "../exceptions/ModelAlreadyExistsException.mjs";
+import ModelAlreadyExistsError from "../exceptions/ModelAlreadyExistsError.mjs";
+import ModelNotFoundError from "../exceptions/ModelNotFoundError.mjs";
+import ParametersError from "../exceptions/ParametersError.mjs";
 import User from "../models/User.mjs";
 
 /**
@@ -12,12 +14,23 @@ export async function createUser ({name, last_name, phone, email, password}) {
     const userExists = await User.findOne({ email });
 
     if (userExists) {
-        throw new ModelAlreadyExists('User');
+        throw new ModelAlreadyExistsError('User');
     }
 
     const user = new User({name, last_name, phone, email, password});
     
     await user.save();
+
+    return user;
+}
+
+export async function getUser (userId) {
+
+    if (! userId) throw ParametersError.missingRequired('userId');
+
+    const user = await User.findOne({_id: userId});
+
+    if (! user) throw new ModelNotFoundError('User');
 
     return user;
 }
