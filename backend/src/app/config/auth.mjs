b7064a93889e3f6ab211cf8a4730ef5b192bb430
Guillermo Rodriguez <guillermo.rodriguez.cv@gmail.com>
2024-06-23 14:18:42 -0600

    refactor(config): using timeToSeconds helper for expires_in in auth config

--- backend/src/app/config/auth.mjs
+++ backend/src/app/config/auth.mjs
@@ -1,13 +1,14 @@
 import 'dotenv/config';
+import { timeToSeconds } from "../utils/dateTimeHelpers.mjs";
 
 export default {
     token : {
         secret_key: process.env.APP_KEY,
-        expires_in: '15m',
+        expires_in: timeToSeconds('15m'),
     },
     refresh_token: {
         secret_key: process.env.APP_REFRESH_KEY,
-        expires_in: '2d',
-        refresh_threshold: (10 * 60) // 10 minutes before expires
+        expires_in: timeToSeconds('2d'),
+        refresh_threshold: timeToSeconds('10m') // 10 minutes before expires
     }
 }
