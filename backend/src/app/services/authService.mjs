d2b9791e4a2e1b0082b7616e039e4277706eb340
Guillermo Rodriguez <guillermo.rodriguez.cv@gmail.com>
2024-06-24 15:17:40 -0600

    chore: add validation rule, exists and unique with asyncRule

--- backend/src/app/services/authService.mjs
+++ backend/src/app/services/authService.mjs
@@ -3,13 +3,15 @@ import ModelAlreadyExistsError from "../errors/ModelAlreadyExistsError.mjs";
 import ResourceNotFoundError from "../errors/ResourceNotFoundError.mjs";
 import User from "../models/User.mjs";
 import RefreshToken from "../models/RefreshToken.mjs";
-import validator from 'validator';
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { jWTokenRequiresRenew, newJWToken, newJWTRefreshToken } from "../utils/jwtHelpers.mjs";
 import authConfig from '../config/auth.mjs';
 import ParametersError from "../errors/ParametersError.mjs";
 import { createRefreshToken } from "../services/refreshTokenService.mjs";
+import validateRequest from "../utils/ValidateRequest.mjs";
+import RequestValidationError from "../errors/RequestValidationError.mjs";
+import Validator from "validatorjs";
 
 /**
  * Store User in Database
@@ -18,24 +20,31 @@ import { createRefreshToken } from "../services/refreshTokenService.mjs";
  * @return user Object
  */
 export async function signupUser ({name, last_name, phone, email, password}) {
-    
-    const userExists = await User.findOne({ email });
-
-    if (userExists) {
-        throw new ModelAlreadyExistsError('User');
-    }
-
-    const user = new User({name, last_name, phone, email, password});
-    
-    await user.save();
-
-    return user;
+    return await validateRequest.validateAsync({
+        request: {name, last_name, phone, email, password},
+        rules:  {
+            name: 'required',
+            last_name: 'required',
+            phone: 'phone:nullable',
+            email: ['required', 'email', {unique: [User, 'email']}],
+            password: 'required'
+        },
+        onPasses: async () => {
+            const user = new User({name, last_name, phone, email, password});
+            
+            await user.save();
+        
+            return user;
+        },
+    });
 }
 
 export async function loginUser (email, password) {
-    if (! email || ! password) throw ParametersError.missingRequired('email', 'password');
-    if (! validator.isEmail(email)) throw ParametersError.invalidFormat('email'); 
-    
+    validateRequest.validate({email, password}, {
+        email: 'required|email', 
+        password: 'required'
+    });
+
     const user = await User.findOne({ email });
     if (! user) throw new ResourceNotFoundError('User');
 
@@ -62,40 +71,46 @@ export async function loginUser (email, password) {
  * @returns 
  */
 export async function refreshUserTokens(refreshToken) {
-    var refreshTokenSignature = jwt.verify(refreshToken, authConfig.refresh_token.secret_key);
+    return validateRequest.validateAsync(
+        {
+            request: {refresh_token: refreshToken},
+            rules: {
+                refresh_token: ['required', 'jwt_verify:refresh_token', {exists: [RefreshToken, 'token']}]
+            },
+            onPasses: async () => {
+                var refreshTokenSignature = jwt.verify(refreshToken, authConfig.refresh_token.secret_key);
 
-    // Try to find in database
-    const existingFreshToken = await RefreshToken.findOne({token: refreshToken});
-    if (! existingFreshToken) throw new ResourceNotFoundError('RefreshToken');
-    
-    const userInformation = {
-        user_id: refreshTokenSignature.user_id, 
-        user_email: refreshTokenSignature.user_email
-    };
+                const userInformation = {
+                    user_id: refreshTokenSignature.user_id, 
+                    user_email: refreshTokenSignature.user_email
+                };
 
-    const refreshTokenRequiresRenew = jWTokenRequiresRenew(refreshTokenSignature, authConfig.refresh_token.refresh_threshold);
+                const refreshTokenRequiresRenew = jWTokenRequiresRenew(refreshTokenSignature, authConfig.refresh_token.refresh_threshold);
 
-    // Evaluates if the actual refreshToken needs to be renewed
-    if (refreshTokenRequiresRenew) {
-        // Overwrite refresh token value
-        refreshToken = newJWTRefreshToken(userInformation.user_id, userInformation.user_email);
+                // Evaluates if the actual refreshToken needs to be renewed
+                if (refreshTokenRequiresRenew) {
+                    // Overwrite refresh token value
+                    refreshToken = newJWTRefreshToken(userInformation.user_id, userInformation.user_email);
 
-        await createRefreshToken(refreshTokenSignature.user_id, refreshToken);
-    }
+                    await createRefreshToken(refreshTokenSignature.user_id, refreshToken);
+                }
 
-    const authPayload = {
-        user_id: signature.user_id,
-        token: newJWToken(userInformation.user_id, userInformation.user_email),
-        refresh_token: refreshToken,
-        refresh_token_was_renewed: refreshTokenRequiresRenew,
-        token_expires_in: authConfig.token.expires_in,
-    };
+                const authPayload = {
+                    user_id: signature.user_id,
+                    token: newJWToken(userInformation.user_id, userInformation.user_email),
+                    refresh_token: refreshToken,
+                    refresh_token_was_renewed: refreshTokenRequiresRenew,
+                    token_expires_in: authConfig.token.expires_in,
+                };
 
-    // Add the expires_in of refresh_token
-    if (authPayload.refresh_token_was_renewed) {
-        authPayload.refresh_token_expires_in = authConfig.refresh_token.expires_in;
-    }
+                // Add the expires_in of refresh_token
+                if (authPayload.refresh_token_was_renewed) {
+                    authPayload.refresh_token_expires_in = authConfig.refresh_token.expires_in;
+                }
 
-    return authPayload;
+                return authPayload;
+            }
+        }
+    );
 }
   
--- backend/src/app/utils/ValidateRequest.mjs
+++ backend/src/app/utils/ValidateRequest.mjs
@@ -1,12 +1,65 @@
 import Validator from 'validatorjs';
 import RequestValidationError from "../errors/RequestValidationError.mjs";
+import authConfig from '../config/auth.mjs';
+import jwt from 'jsonwebtoken';
+
+Validator.register('jwt_verify', function(value, tokenConfig, attribute) { 
+    try {
+        jwt.verify(value, authConfig[tokenConfig].secret_key);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}, 'The :attribute is not a valid token.');
+
+Validator.register('phone', (value, requirement, attribute) => {
+    // Pass if is null or undefined
+    if (requirement === 'nullable' && ! value) return true;
+
+    // Must be a number and have 10 digits
+    return /^\d{10}$/.test(value);
+}, 'The :attribute is not a valid phone number!')
+
+Validator.registerAsync('exists', async (value, requirement, attribute, passes) => {
+    if (! value) return true;
+    const [Model, column] = requirement;
+    const found = await Model.exists({[column]: value});
+    passes(found !== null, `The ${attribute} not exists!`)
+});
+
+
+Validator.registerAsync('unique', async (value, requirement, attribute, passes) => {
+    if (! value) return true;
+    const [Model, column] = requirement;
+    const found = await Model.exists({[column]: value});
+    passes(found === null, `The ${attribute} already exists!`)
+});
 
 export default {
-    validate: (request, rules, messages) => {
-        const validation = new Validator(request, rules, messages);
+    validateAsync: ({ request, rules, messages, onPasses, onFails }) => {
+        const validator = new Validator(request, rules, messages);
+        validator.stopOnError(true);
 
-        if (validation.fails()) {
-            throw new RequestValidationError(validation);
+        return new Promise((resolve, reject) => {
+            validator.checkAsync(
+                async () => {
+                    const result = await onPasses();
+                    resolve(result);
+                }, 
+                () => reject(new RequestValidationError(validator))
+            );
+        });
+    },
+
+    validate: (request, rules, messages) => {
+        const validator = new Validator(request, rules, messages);
+
+        validator.stopOnError(true);
+
+        if (validator.fails()) {
+            throw new RequestValidationError(validator);
         }
+
+        return request;
     }
 };
