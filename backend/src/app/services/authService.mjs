772357de3ee19391559cc593648a40723c439b16
Guillermo Rodriguez <guillermo.rodriguez.cv@gmail.com>
2024-06-24 15:28:57 -0600

    using async validation rules in authService

--- backend/src/app/services/authService.mjs
+++ backend/src/app/services/authService.mjs
@@ -1,17 +1,12 @@
 import AuthValidationError from "../errors/AuthValidationError.mjs";
-import ModelAlreadyExistsError from "../errors/ModelAlreadyExistsError.mjs";
-import ResourceNotFoundError from "../errors/ResourceNotFoundError.mjs";
 import User from "../models/User.mjs";
 import RefreshToken from "../models/RefreshToken.mjs";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { jWTokenRequiresRenew, newJWToken, newJWTRefreshToken } from "../utils/jwtHelpers.mjs";
-import authConfig from '../config/auth.mjs';
-import ParametersError from "../errors/ParametersError.mjs";
+import authConfig from '../config/auth.mjs'
 import { createRefreshToken } from "../services/refreshTokenService.mjs";
 import validateRequest from "../utils/ValidateRequest.mjs";
-import RequestValidationError from "../errors/RequestValidationError.mjs";
-import Validator from "validatorjs";
 
 /**
  * Store User in Database
@@ -20,35 +15,38 @@ import Validator from "validatorjs";
  * @return user Object
  */
 export async function signupUser ({name, last_name, phone, email, password}) {
-    return await validateRequest.validateAsync({
-        request: {name, last_name, phone, email, password},
-        rules:  {
-            name: 'required',
-            last_name: 'required',
-            phone: 'phone:nullable',
-            email: ['required', 'email', {unique: [User, 'email']}],
-            password: 'required'
-        },
-        onPasses: async () => {
-            const user = new User({name, last_name, phone, email, password});
-            
-            await user.save();
-        
-            return user;
-        },
-    });
+    await validateRequest.validateAsync(
+        {
+            request: {name, last_name, phone, email, password},
+            rules:  {
+                name: 'required',
+                last_name: 'required',
+                phone: 'phone:nullable',
+                email: ['required', 'email', { unique: [User, 'email'] }],
+                password: 'required'
+            },
+        }
+    );
+
+    const user = new User({name, last_name, phone, email, password});
+    await user.save();
+    return user;
 }
 
 export async function loginUser (email, password) {
-    validateRequest.validate({email, password}, {
-        email: 'required|email', 
-        password: 'required'
-    });
+    await validateRequest.validateAsync(
+        {
+            request: {email, password},
+            rules: {
+                email: ['required', 'email', { exists: [User, 'email'] }],
+                password: 'required',
+            },
+        }
+    );
 
     const user = await User.findOne({ email });
-    if (! user) throw new ResourceNotFoundError('User');
-
     const isMatch = await bcrypt.compare(password, user.password);
+
     if (! isMatch) throw AuthValidationError.wrongPassword();
 
     const authPayload = {
@@ -71,46 +69,45 @@ export async function loginUser (email, password) {
  * @returns 
  */
 export async function refreshUserTokens(refreshToken) {
-    return validateRequest.validateAsync(
+    await validateRequest.validateAsync(
         {
             request: {refresh_token: refreshToken},
             rules: {
                 refresh_token: ['required', 'jwt_verify:refresh_token', {exists: [RefreshToken, 'token']}]
-            },
-            onPasses: async () => {
-                var refreshTokenSignature = jwt.verify(refreshToken, authConfig.refresh_token.secret_key);
-
-                const userInformation = {
-                    user_id: refreshTokenSignature.user_id, 
-                    user_email: refreshTokenSignature.user_email
-                };
-
-                const refreshTokenRequiresRenew = jWTokenRequiresRenew(refreshTokenSignature, authConfig.refresh_token.refresh_threshold);
-
-                // Evaluates if the actual refreshToken needs to be renewed
-                if (refreshTokenRequiresRenew) {
-                    // Overwrite refresh token value
-                    refreshToken = newJWTRefreshToken(userInformation.user_id, userInformation.user_email);
-
-                    await createRefreshToken(refreshTokenSignature.user_id, refreshToken);
-                }
-
-                const authPayload = {
-                    user_id: signature.user_id,
-                    token: newJWToken(userInformation.user_id, userInformation.user_email),
-                    refresh_token: refreshToken,
-                    refresh_token_was_renewed: refreshTokenRequiresRenew,
-                    token_expires_in: authConfig.token.expires_in,
-                };
-
-                // Add the expires_in of refresh_token
-                if (authPayload.refresh_token_was_renewed) {
-                    authPayload.refresh_token_expires_in = authConfig.refresh_token.expires_in;
-                }
-
-                return authPayload;
             }
         }
     );
+
+    var refreshTokenSignature = jwt.verify(refreshToken, authConfig.refresh_token.secret_key);
+
+    const userInformation = {
+        user_id: refreshTokenSignature.user_id, 
+        user_email: refreshTokenSignature.user_email
+    };
+
+    const refreshTokenRequiresRenew = jWTokenRequiresRenew(refreshTokenSignature, authConfig.refresh_token.refresh_threshold);
+
+    // Evaluates if the actual refreshToken needs to be renewed
+    if (refreshTokenRequiresRenew) {
+        // Overwrite refresh token value
+        refreshToken = newJWTRefreshToken(userInformation.user_id, userInformation.user_email);
+
+        await createRefreshToken(refreshTokenSignature.user_id, refreshToken);
+    }
+
+    const authPayload = {
+        user_id: signature.user_id,
+        token: newJWToken(userInformation.user_id, userInformation.user_email),
+        refresh_token: refreshToken,
+        refresh_token_was_renewed: refreshTokenRequiresRenew,
+        token_expires_in: authConfig.token.expires_in,
+    };
+
+    // Add the expires_in of refresh_token
+    if (authPayload.refresh_token_was_renewed) {
+        authPayload.refresh_token_expires_in = authConfig.refresh_token.expires_in;
+    }
+
+    return authPayload;
 }
   
