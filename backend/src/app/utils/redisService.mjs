ec1ae7b213871c3012ff1516e23e6965001dd043
Guillermo Rodriguez <guillermo.rodriguez.cv@gmail.com>
2024-06-29 20:48:19 -0600

    chore: renamed redisClient to redisService

--- backend/src/app/utils/redisService.mjs
+++ backend/src/app/utils/redisService.mjs
@@ -15,6 +15,24 @@ export async function connectToRedis() {
         .catch((err) => console.error('Error connecting to Redis:', err));
 }
 
+export async function saveToRedis(key, data) {
+    const success = await client.set(key, JSON.stringify(data));
+
+    return success === 'OK';
+}
+
+export async function getFromRedis(key) {
+    const data = await client.get(key);
+    
+    return JSON.parse(data);
+}
+
+export async function deleteFromRedis(key) {
+    const result = await client.del(key);
+    
+    return result > 0; 
+}
+
 client.on('error', (err) => {
     console.error('Error connecting to Redis:', err);
 });
